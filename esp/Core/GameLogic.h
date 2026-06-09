5c618de4fb53d9b1b71914190abcf6eeaa52c7c5
LLOS Lord <chungphan1607@gmail.com>
2026-06-09 17:13:20 +0700

    Update GameLogic.h

--- esp/Core/GameLogic.h
+++ esp/Core/GameLogic.h
@@ -1,22 +1,27 @@
-#ifndef GameLogic_h
-#define GameLogic_h
+#pragma once
+#include <stdint.h>
 
-#import "MemoryUtils.h"
-#import "UnityMath.h"
+struct PlayerID {
+    uint64_t m_PlayerID;  // 0x00
+    int m_TeamID;         // 0x08
+};
 
-#pragma mark - Function Game
-
-uint64_t getMatchGame(uint64_t Moudule_Base);
+uint64_t getMatchGame(uint64_t module_base);
 uint64_t getMatch(uint64_t matchgame);
 uint64_t CameraMain(uint64_t matchgame);
-float* GetViewMatrix(uint64_t cameraMain);
-uint64_t getTransNode(uint64_t BodyPart);
+float* GetViewMatrix(uint64_t camera);
+uint64_t getLocalPlayer(uint64_t match);
+bool isLocalTeamMate(uint64_t localPlayer, uint64_t otherPlayer);
+
+// HP (trực tiếp từ dump)
+int get_CurHP(uint64_t player);
+int get_MaxHP(uint64_t player);
+
+// Bone (cần cập nhật offset chính xác khi có)
 uint64_t getHead(uint64_t player);
 uint64_t getRightToeNode(uint64_t player);
-uint64_t getLocalPlayer(uint64_t match);
-int GetDataUInt16(uint64_t player, int varID);
-int get_CurHP(uint64_t Player);
-int get_MaxHP(uint64_t Player);
-bool isLocalTeamMate(uint64_t localPlayer, uint64_t Player);
 
-#endif
+// Memory helpers
+template<typename T>
+T ReadAddr(uint64_t addr);
+bool isVaildPtr(uint64_t ptr);
